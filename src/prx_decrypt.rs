e6546232ac2e665e6f552a3549c1e1ae1be63b3d
0xLozi <augustoguerrero.ai@gmail.com>
2026-05-26 15:52:56 -0300

    testing more

--- src/prx_decrypt.rs
+++ src/prx_decrypt.rs
@@ -9,16 +9,18 @@ pub fn decrypt_prx(binario_eboot: &[u8]) {
     let tag: u32 = u32::from_le_bytes(bytes_tag_offset);
 
     // como no sabemos el PRX type pues toca desencriptarlo de manera brute-force
-    let resultado = pspDecryptTipo0(binario_eboot);
+    let resultado = pspDecryptTipo0(&binario_eboot);
 
-    
-    
-    
 }
 
 
 
+
 pub fn pspDecryptTipo0(binario_eboot: &[u8]) {
-    
+    // el size a desencriptar para los de tipo 0 se encuentra en 0xB0
+    let offset_size_slice = &binario_eboot[0xB0 .. 0xB0 + 4];
+    let arreglo_fijo: [u8;4] = offset_size_slice.try_into().unwrap();
+    let decrypt_size = u32::from_le_bytes(arreglo_fijo);
+    println!("Tamaño a desencriptar: {}", decrypt_size);
 
 }
